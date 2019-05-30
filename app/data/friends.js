457ccbe84df756063addc5b804698e74bb9d8851
Sculpin <Loungelizard96@Neds-MacBook-Pro.local>
2019-05-30 16:12:38 -0700

    added files to github

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+let friendsList = [
+    {
+        name: "Bruce Wayne",
+        photo: "http://images.shirts.com/products/28910/14-14-56645/bruce-wayne-for-president-t-shirt-alt1.jpg",
+        scores: [
+            4,
+            3,
+            5,
+            3,
+            2,
+            1,
+            3,
+            2,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Clark Kent",
+        photo: "https://vignette.wikia.nocookie.net/marvelcrossroads/images/c/c8/Clark_Kent_EUM.jpg/revision/latest?cb=20151030214353",
+        scores: [
+            3,
+            2,
+            1,
+            4,
+            5,
+            4,
+            4,
+            2,
+            3,
+            3
+        ]
+    },
+];
+
+module.exports = friendsList;
